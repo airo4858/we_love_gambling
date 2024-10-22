2694441bbdbc9e50b7dca1f95113f93a734c2827
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2024-10-22 14:54:07 -0600

    jackpot animation

--- main.gd
+++ main.gd
@@ -11,9 +11,15 @@ func _input(event):
 		if (event.button_index == 1 and event.is_pressed()):
 			if gambling_state:
 				gambling_state = false
-				slot1_number = randf()*7
-				slot2_number = randf()*7
-				slot3_number = randf()*7
+				slot1_number = randf()*7 + 1
+				print("Slot1 = ")
+				print(slot1_number)
+				slot2_number = randf()*7 + 1
+				print("Slot2 = ")
+				print(slot2_number)
+				slot3_number = randf()*7 + 1
+				print("Slot3 = ")
+				print(slot3_number)
 				get_node("Phone").get_node("AnimationPhone").play("yes_gambling")
 				money -= 10
 				get_tree().get_root().get_node("Main/HUD").go_gambling(10)
@@ -64,31 +70,31 @@ func _input(event):
 					get_node("Slot3").get_node("AnimationSlots").play("running_7")
 				#Gambling Logic
 				if (slot1_number == 7 and slot2_number == 7 and slot3_number == 7):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(1000)
 					money += 1000
 				elif (slot1_number == 6 and slot2_number == 6 and slot3_number == 6):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(500)
 					money += 500
 				elif (slot1_number == 5 and slot2_number == 5 and slot3_number == 5):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(250)
 					money += 250
 				elif (slot1_number == 4 and slot2_number == 4 and slot3_number == 4):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(200)
 					money += 200
 				elif (slot1_number == 3 and slot2_number == 3 and slot3_number == 3):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(150)
 					money += 150
 				elif (slot1_number == 2 and slot2_number == 2 and slot3_number == 2):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(125)
 					money += 125
 				elif (slot1_number == 1 and slot2_number == 1 and slot3_number == 1):
-					#PLAY JACKPOT ANIMATION
+					$JackpotAnimation.play("jackpot_animation")
 					get_tree().get_root().get_node("Main/HUD").big_winning(100)
 					money += 100
 				#Losing Code
